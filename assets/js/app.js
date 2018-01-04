f5c8ab6f6c71f5714eb03c6393f291de97a1548a
Kris <christellelocque@hotmail.fr>
2018-01-04 11:24:37 +0100

    After a part of bonus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,37 @@
 console.log('connect')
 
-alert("Welcome!");
+//alert("Welcome!");
 
 var bouton = document.getElementById("bouton"); //je capture mon bouton
 var ulToDo = document.getElementById("list"); //je capture mon élément ul dans le html
 var i = 0; //pr incrémenter chaque id
+var stockage = localStorage;
 
 bouton.addEventListener("click", addTask); //quand je clique sur le bouton, j'exécute la fonction ci-dessous
 
 function addTask() {
     var input = document.getElementById("search").value; //je récupère dans une variable la valeur de mon input
+
+    if (input==0) { //si l'input est nul (non renseigné) 
+        alert("Tâche non valide");
+        return false; //alors n'exécutera pas la fonction
+    };
+
+    for ( var i = 0, len = stockage.length; i < len; i++ ) { //pour chaque clé du localstorage
+        console.log( stockage.key( i ) ) ;
+        if (input==stockage.key(i)) { //si l'input rentré est identique à une clé existante
+            alert("Tâche présente");
+            return false; //alors la fonction ne s'exécutera pas
+        }
+       }
+
+
     var dateDeb = new Date(); //création d'une date/horaire de début(création de task)
 
     var liste = document.createElement('li'); //je créé des éléments de liste 'li' pr chq fction lancée
     var check = document.createElement('input'); //idem pour les inputs
     var label = document.createElement('label'); //idem pr les labels
-    var heure = document.createElement('span'); //idem pr le span contenant l'heure
+    var heure = document.createElement('p'); //idem pr le span contenant l'heure
     var supp = document.createElement('a');
 
     liste.setAttribute("id", "elmtLi" + i);
@@ -33,7 +49,7 @@ function addTask() {
 
     label.innerHTML = input; //place la valeur de l'input (ajout tâche) dans le label
     i++; //va permettre d'incrémenter de 1 mes 2 i placés au-dessus
-    heure.innerHTML = "   " + dateDeb.toLocaleDateString("fr-FR") + " - " + dateDeb.toLocaleTimeString("fr-FR") + "   ";//place l'heure dans le html avec une simplification permettant de la rendre lisible
+    heure.innerHTML = dateDeb.toLocaleDateString("fr-FR") + " - " + dateDeb.toLocaleTimeString("fr-FR") + "   ";//place l'heure dans le html avec une simplification permettant de la rendre lisible
     supp.innerHTML = "Supprimer";
 
     liste.appendChild(check); //attribution du check à mon li
@@ -46,15 +62,13 @@ function addTask() {
     var okTask = check.addEventListener("click", doTask); //quand check est cliqué, il exécute la fonction suivante
     
     function doTask() {
-        if(check.checked) {
-            document.getElementById(input).style.textDecoration="line-through"; //sélection du label et lui attribuer un css pour rayer
-        }else {
-            document.getElementById(input).style.textDecoration="none";
+        if(check.checked) { //si le bouton est coché
+            document.getElementById(input).style.textDecoration="line-through"; //alors sélection du label et lui attribuer un css pour rayer
+        }else { 
+            document.getElementById(input).style.textDecoration="none"; //sinon il n'est pas rayé
         }
         
         var dateFin = new Date(); //création d'une date/horaire de fin(exécution de la task)
-        //var heureFin = dateFin.toLocaleDateString("fr-FR") + " " + dateFin.toLocaleTimeString("fr-FR"); //lisibilité de l'horaire
-        //console.log(heureFin);
 
         temps = dateFin.getTime() - dateDeb.getTime(); //calcul du temps écoulé entre les 2 fonctions de création et de réalisation
         console.log(temps/1000);
@@ -67,6 +81,7 @@ function addTask() {
     function doSupp() {
         document.getElementById("list").removeChild(liste); //supprime les élèments li de la ul
         stockage.removeItem(input);
+        alert("Tâche supprimée");
     };
     
     var restDo = document.getElementById('test1'); //sélection du bouton reste à faire
@@ -96,10 +111,13 @@ function addTask() {
 
     function filtre3() {
             document.getElementById(input).parentElement.style.display = ""; //affiche toutes les tâches par défaut
-    }
+    };
         //création du stockage des tâches dans le localstorage
-        var stockage = localStorage;
         stockage.setItem(input, dateDeb);
+
+    window.onload = function session() {
+        
+    };
 };
 
 
